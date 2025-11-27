4b11b1fba5601b66cf956f9fe7ee63d790a870cf
zhaodong-liu <151713377+zhaodong-liu@users.noreply.github.com>
2025-11-27 13:27:19 +0000

    Deploying to gh-pages from @ zhaodong-liu/zhaodong-liu.github.io@279b693592ab7c786d7e35d85a74026617838de3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "projects",
           description: "My projects",
           section: "Navigation",
